bbde3bbc6a4d54806ba608c641a4a82143ef9504
ImJustNon <Im_Just_Non@hotmail.com>
2025-09-12 03:19:31 +0700

    Feat : Preview before download

--- src/app/api/v1/pep/download/route.ts
+++ src/app/api/v1/pep/download/route.ts
@@ -1,7 +1,5 @@
-// src/app/api/v1/pep/download/route.ts
 import { NextResponse } from "next/server";
 import axios from "axios";
-import { useSearchParams } from "next/navigation";
 
 export async function GET(req: Request): Promise<NextResponse> {
     const { searchParams } = new URL(req.url);
@@ -16,41 +14,49 @@ export async function GET(req: Request): Promise<NextResponse> {
         });
     }
 
-    // if(!name) name = "kmutt-pep.pdf";
+    if(!name) name = "kmutt-pep.pdf";
+
 
     try {
-        return NextResponse.redirect(url);
+        // Fetch for SAML SimpleSAMLAuthToken cookie
+        const samlResponse = await axios.get("https://auto-saml-kmutt.onrender.com/api/saml/request", {
+            timeout: 30 * 1000,
+        });
+        if(!samlResponse.status || samlResponse.status !== 200){
+            throw new Error(samlResponse.data.message);
+        }
+        
 
-        // const response = await axios.get(url, {
-        //     responseType: "arraybuffer",
-        //     headers: {
-        //         "Accept":
-        //         "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        //         "Accept-Language": "en-US,en;q=0.9,th-TH;q=0.8,th;q=0.7",
-        //         "User-Agent":
-        //         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-        //         "Referer": "https://dl.lib.kmutt.ac.th/",
-        //         "Cookie":
-        //         "SimpleSAMLphp=pgcaih5ejohdop8901n2o6l37n; SimpleSAMLAuthToken=_d28d9905c5df84c3ae162c955b8ecc805cdcffa2b9;",
-        //     },
-        // });
+        const response = await axios.get(url, {
+            responseType: "arraybuffer",
+            headers: {
+                "Accept":
+                "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                "Accept-Language": "en-US,en;q=0.9,th-TH;q=0.8,th;q=0.7",
+                "User-Agent":
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+                "Referer": "https://dl.lib.kmutt.ac.th/",
+                "Cookie":
+                `SimpleSAMLphp=${samlResponse.data.data.simpleSAMLphp};SimpleSAMLAuthToken=${samlResponse.data.data.simpleSAMLAuthToken};`,
+            },
+        });
 
-        // const buffer = Buffer.from(new Uint8Array(response.data));
+        const buffer = Buffer.from(new Uint8Array(response.data));
 
-        // const encodedName = encodeURIComponent(name);
+        const encodedName = encodeURIComponent(name);
 
-        // return new NextResponse(buffer, {
-        //     status: 200,
-        //     headers: {
-        //         "Content-Type": "application/pdf",
-        //         "Content-Disposition": `inline; filename=${encodedName}`,
-        //     },
-        // });
+        return new NextResponse(buffer, {
+            status: 200,
+            headers: {
+                "Content-Type": "application/pdf",
+                "Content-Disposition": `inline; filename=${encodedName}`,
+            },
+        });
     } catch (err: any) {
-        console.error("Download error:", err.message);
         return NextResponse.json({ 
-            error: "Failed to fetch PDF", 
-            details: err.message 
+            error: "Internal Server Error", 
+            message: err.message ,
+            targetUrl: url
         }, { 
             status: 500 
         });
